7e00340aba0b8c70686c121b2f6379fd792cfe59
Sara Gorecki <sgorecki@gmail.com>
2014-01-21 22:48:19 -0500

    fix syntax for dice spec

--- dice/spec/DiceSpec.js
+++ dice/spec/DiceSpec.js
@@ -2,14 +2,14 @@ describe( "Dice#roll", function() {
   dice = new Dice();
 
   it("should return an number", function() {
-    expect(typeof dice.roll()).toBe("number")
+    expect(typeof dice.roll()).toBe("number");
   });      
 
   it("should return a number greater than 0", function() {
-    expect(dice.roll()).toBeGreaterThan(0)
+    expect(dice.roll()).toBeGreaterThan(0);
   });  
 
   it("should return a number less than 7", function() {
-    expect(dice.roll()).toBeLessThan(7)
+    expect(dice.roll()).toBeLessThan(7);
   });  
 });
