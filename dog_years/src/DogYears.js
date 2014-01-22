84e1c0fd0fbbbe56744e83ed8ef2f2b23cf8e924
Sara Gorecki <sgorecki@gmail.com>
2014-01-21 23:01:22 -0500

    add code for dog years

--- dog_years/src/DogYears.js
+++ dog_years/src/DogYears.js
@@ -1,2 +1,18 @@
 //create an object called dog
 //it should have 3 functions, ageInYears, ageInDays, ageInDogYears
+
+function Dog(){
+  this.age = Math.floor(Math.random()*20 + 1);
+}
+
+Dog.prototype.ageInYears = function(){
+  return this.age;
+};
+
+Dog.prototype.ageInDays = function(){
+  return this.age * 365;
+};
+
+Dog.prototype.ageInDogYears = function(){
+  return this.age * 7;
+};
